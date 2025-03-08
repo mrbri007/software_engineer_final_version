9c5cb1b89115373283a007cdb01cedf8ed33abb6
“Your <brigupta089@gmail.com>
2025-03-08 18:22:35 +0530

    Initial commit

---
+++ backend/controllers/post.js
@@ -0,0 +1,20 @@
+const Post = require("../models/Post");
+
+exports.createPost = async (req, res) => {
+  try {
+    const post = await new Post(req.body).save();
+    res.json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate("user", "first_name last_name pciture username gender")
+      .sort({ createdAt: "desc" });
+    res.json(posts);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
---
+++ frontend/src/App.js
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { useReducer } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import CreatePostPopup from "./components/createPostPopup";
+import { postsReducer } from "./functions/reducers";
+import Home from "./pages/home";
+import Activate from "./pages/home/activate";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+import Reset from "./pages/reset";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
+import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
+
+function App() {
+  const [visible, setVisible] = useState(false);
+  const { user } = useSelector((state) => ({ ...state }));
+  const [{ loading, error, posts }, dispatch] = useReducer(postsReducer, {
+    loading: false,
+    posts: [],
+    error: "",
+  });
+  useEffect(() => {
+    getAllPosts();
+  }, []);
+
+  const getAllPosts = async () => {
+    try {
+      dispatch({
+        type: "POSTS_REQUEST",
+      });
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/getAllPosts`,
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      dispatch({
+        type: "POSTS_SUCCESS",
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "POSTS_ERROR",
+        payload: error.response.data.message,
+      });
+    }
+  };
+  return (
+    <div>
+      {visible && <CreatePostPopup user={user} setVisible={setVisible} />}
+      <Routes>
+        <Route element={<LoggedInRoutes />}>
+          <Route
+            path="/profile"
+            element={<Profile setVisible={setVisible} />}
+            exact
+          />
+          <Route
+            path="/profile/:username"
+            element={<Profile setVisible={setVisible} />}
+            exact
+          />
+          <Route
+            path="/"
+            element={<Home setVisible={setVisible} posts={posts} />}
+            exact
+          />
+          <Route path="/activate/:token" element={<Activate />} exact />
+        </Route>
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path="/login" element={<Login />} exact />
+        </Route>
+        <Route path="/reset" element={<Reset />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
